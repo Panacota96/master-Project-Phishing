9e732cda180ce2ffb03c73f77aee8ea06bcfdee2
Panacota96 <david_gonzalez@javeriana.edu.co>
2026-03-21 12:27:59 +0100

    feat: add lambda:GetFunctionCodeSigningConfig permission to GitHub Actions IAM role policy

--- terraform/github_actions_oidc.tf
+++ terraform/github_actions_oidc.tf
@@ -56,6 +56,7 @@ resource "aws_iam_role_policy" "github_actions_deploy" {
           "lambda:GetEventSourceMapping",
           "lambda:UpdateEventSourceMapping",
           "lambda:ListEventSourceMappings",
+          "lambda:GetFunctionCodeSigningConfig",
         ]
         Resource = "*"
       },
